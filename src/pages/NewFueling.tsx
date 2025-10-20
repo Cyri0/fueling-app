717273ddfe15c9e5c406d24ef6783da11e4155a7
NT - Burg <you@example.com>
2025-10-20 14:24:04 +0200

    Refactor NewFueling form: add state management and handle form submission

--- src/pages/NewFueling.tsx
+++ src/pages/NewFueling.tsx
@@ -1,25 +1,38 @@
-import { useState } from "react"
+import { useState, type FormEvent } from "react"
+
+type FuelEventType = {
+    km: number,
+    volume: number,
+    fuelType: string,
+    price: number
+}
 
 const NewFueling = () => {
-    const [km, setKm] = useState()
-    const [volume, setVolume] = useState()
-    const [fuelType, setFuelType] = useState()
-    const [price, setPrice] = useState()
+    const [km, setKm] = useState<number>(0)
+    const [volume, setVolume] = useState<number>(0)
+    const [fuelType, setFuelType] = useState<string>("95")
+    const [price, setPrice] = useState<number>(0)
+
+    const formSubmitted = (e: FormEvent) => {
+        e.preventDefault()
+        const currentData:FuelEventType  = { km, volume, fuelType, price }
+        console.log(currentData)
+    }
 
     return (
         <section>
-            <form>
+            <form onSubmit={formSubmitted}>
                 <div>
                     <label>km</label>
-                    <input type="number" />
+                    <input type="number" value={km} onChange={(e) => setKm(Number(e.target.value))} />
                 </div>
 
                 <div>
                     <label>volume</label>
-                    <input type="number" /><span>l</span></div>
+                    <input type="number" value={volume} onChange={(e) => setVolume(Number(e.target.value))} /><span>l</span></div>
                 <div>
                     <label>fuel type</label>
-                    <select>
+                    <select value={fuelType} onChange={(e) => setFuelType(e.target.value)}>
                         <option value={"95"}>95</option>
                         <option value={"100"}>100</option>
                         <option value={"diesel"}>diesel</option>
@@ -28,7 +41,7 @@ const NewFueling = () => {
 
                 <div>
                     <label>price</label>
-                    <input type="number" /><span>HUF</span>
+                    <input type="number" value={price} onChange={(e) => setPrice(Number(e.target.value))} /><span>HUF</span>
                 </div>
 
                 <button type="submit">
